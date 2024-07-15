145c15f92212f2ed54783cb6624daaa82f0ba9bf
Kitsune Oblivion <161637706+JPSSandes@users.noreply.github.com>
2024-07-15 13:00:23 -0300

    Voltei com algumas correções no código

---
+++ Main.kt
@@ -0,0 +1,45 @@
+fun main() {
+
+    /* Estruturas de Dados Estáticas */
+
+    // Pilha Estática Normal
+    val pilhaEstatica = PilhaEstatica(3);
+    pilhaEstatica.empilhar("Untouchables")
+    pilhaEstatica.empilhar("Hybrid Theory")
+    pilhaEstatica.empilhar("White Pony")
+
+    // Pilha Estática Invertida
+    val pilhaEstaticaInvertida = PilhaEstaticaInvertida(3)
+    pilhaEstaticaInvertida.empilhar("Untouchables")
+    pilhaEstaticaInvertida.empilhar("Hybrid Theory")
+    pilhaEstaticaInvertida.empilhar("White Pony")
+
+    // Pilha Estática Dupla
+    val pilhaEstaticaDupla = PilhaEstaticaDupla(6)
+    pilhaEstaticaDupla.empilhar1("Untouchables");
+    pilhaEstaticaDupla.empilhar1("Hybrid Theory")
+    pilhaEstaticaDupla.empilhar1("White Pony")
+    pilhaEstaticaDupla.empilhar2("Issues")
+    pilhaEstaticaDupla.empilhar2("Meteora")
+    pilhaEstaticaDupla.empilhar2("Around the Fur")
+
+    // Fila
+    val filaEstatica = FilaEstatica(3)
+    filaEstatica.enfileirar("Here to Stay")
+    filaEstatica.enfileirar("Points of Authority")
+    filaEstatica.enfileirar("Digital Bath")
+
+    // Fila Circular
+    val filaEstaticaCircular = FilaEstaticaCircular(3)
+    filaEstaticaCircular.enfileirar("Here to Stay")
+    filaEstaticaCircular.enfileirar("Points of Authority")
+    filaEstaticaCircular.enfileirar("Digital Bath")
+
+    // Lista
+    val listaEstaticaCircular = ListaEstaticaCircular(3)
+    listaEstaticaCircular.anexar("Here to Stay")
+    listaEstaticaCircular.anexar("Points of Authority")
+    listaEstaticaCircular.anexar("Digital Bath")
+
+    /* Estruturas de Dados Dinâmicas */
+}
---
+++ out/production/estrutura-dados/README.md
@@ -0,0 +1,3 @@
+# Estrutura-de-Dados
+
+Uma pasta para trabalhos e atividades envolvendo a faculdade, mais especificamente em Estrutura de Dados
